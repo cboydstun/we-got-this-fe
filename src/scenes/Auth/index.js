23df0355c9e7735f9fdbde5409d0b77909935e42
Lucas Bazemore <lucasabazemore@gmail.com>
2019-11-12 23:09:27 -0600

    added row and column components

--- src/scenes/Auth/index.js
+++ src/scenes/Auth/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useStateValue } from '../../state';
 import { actions } from '../../state/auth/authActions';
-import { Route, Link, useHistory } from 'react-router-dom';
-import { RegisterCompany } from '../../components';
+import { useHistory } from 'react-router-dom';
 import { routes } from '../../constants/routes';
+import Column from '../../components/styles/containers/Column';
+import Row from '../../components/styles/containers/Row';
 
 const Auth = () => {
     const [{ auth }, dispatch] = useStateValue();
@@ -11,34 +12,43 @@ const Auth = () => {
 
     return (
         <>
-            <h1>Auth</h1>
-            {auth.loadingUser ? (
-                <h4>Loading</h4>
-            ) : (
-                <h4>{auth.currentUser && auth.currentUser.displayName}</h4>
-            )}
-            <button
-                onClick={async () => {
-                    let result = await actions.login(dispatch);
-                    console.log(result);
-                    if (result == true) {
-                        history.push(routes.HOME);
-                    }
-                }}
-            >
-                Sign In With Google
-            </button>
-            <button
-                onClick={async () => {
-                    let result = await actions.login(dispatch);
-                    console.log(result);
-                    if (result == true) {
-                        history.push(routes.AUTH_REGISTER_COMPANY);
-                    }
-                }}
-            >
-                Register With Google
-            </button>
+            <Row>
+                <Column justify="center" align="center" padding={20}>
+                    <h1>Auth</h1>
+                    {auth.loadingUser ? (
+                        <h4>Loading</h4>
+                    ) : (
+                        <h4>
+                            {auth.currentUser && auth.currentUser.displayName}
+                        </h4>
+                    )}
+                    <button
+                        onClick={async () => {
+                            let result = await actions.login(dispatch);
+                            console.log(result);
+                            if (result == true) {
+                                history.push(routes.HOME);
+                            }
+                        }}
+                    >
+                        Sign In With Google
+                    </button>
+                    <button
+                        onClick={async () => {
+                            let result = await actions.login(dispatch);
+                            console.log(result);
+                            if (result == true) {
+                                history.push(routes.AUTH_REGISTER_COMPANY);
+                            }
+                        }}
+                    >
+                        Register With Google
+                    </button>
+                </Column>
+                <Column justify="center" align="center" padding={20}>
+                    <div>Here is some text!</div>
+                </Column>
+            </Row>
         </>
     );
 };
