c0a86aca2b624e155779202a4701a6635c27bdff
sanny-io <sannysherief@gmail.com>
2019-11-13 15:14:14 -0800

    More tech work.

--- src/scenes/Techs/TechCard.js
+++ src/scenes/Techs/TechCard.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import { Card } from '@material-ui/core';
+import { Card, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
-    card: {
-        '& h3': {
-            color: 'red',
-        },
-    
-        'background': 'red',
-    }
-    
-});
+const useStyles = makeStyles(theme => ({
+    personalInfo: {
+        display: 'flex',
 
-const TechCard = ({ displayName, photoUrl }) => {
+        '& .photo': {
+            maxWidth: '150px',
+            marginRight: theme.spacing(2)
+        }
+    },
+
+    employmentInfo: {
+        padding: theme.spacing(2),
+    },
+
+    controls: {
+        textAlign: 'right',
+    },
+}));
+
+const TechCard = ({ displayName, photoUrl, address, city, state, zip, phone }) => {
     const classes = useStyles();
+
     return (
-        <Card className={classes.card}>
-            <img src={photoUrl} alt='Test' />
-            <h3>{displayName}</h3>
+        <Card>
+            <div className={classes.personalInfo}>
+                <img className="photo" src={photoUrl} alt={displayName} />
+                <div>
+                    <h2>{displayName}</h2>
+                    <span>{address}</span>
+                    <br />
+                    <span>{city}, {state} {zip}</span>
+                    <br />
+                    <span>{phone}</span>
+                </div>
+            </div>
+            <div className={classes.employmentInfo}>
+                <div className={classes.controls}>
+                    <Button variant="contained">Edit</Button>
+                </div>
+            </div>
         </Card>
     );
 };
--- src/scenes/Techs/index.js
+++ src/scenes/Techs/index.js
@@ -1,72 +1,53 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { Paper, Grid } from '@material-ui/core';
 import TechCard from './TechCard';
 
-const useStyles = makeStyles(theme => ({
-    techs: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        margin: '0',
-        padding: '0',
-        listStyle: 'none',
-        justifyContent: 'space-between',
-    },
-    techWrapper: {
-        // marginLeft: -theme.spacing(2),
-        // marginRight: -theme.spacing(2),
-        marginBottom: theme.spacing(2),
-        width: '48%',
-    },
-    tech: {
-        // paddingLeft: theme.spacing(2),
-        // paddingRight: theme.spacing(2),
-    },
-}));
-
 const Techs = ({ techs }) => {
-    const styles = useStyles();
-
     techs = [
-        { 
+        {
             displayName: 'Sanny Sherief',
             photoUrl: 'https://i.imgur.com/l5YJgol.png',
             address: '123 Fake Street',
             city: 'Los Angeles',
             state: 'CA',
             zip: '90019',
+            phone: '3233233232',
         },
-        { 
+        {
             displayName: 'Sanny Sherief',
             photoUrl: 'https://i.imgur.com/l5YJgol.png',
             address: '123 Fake Street',
             city: 'Los Angeles',
             state: 'CA',
             zip: '90019',
+            phone: '3233233232',
         },
-        { 
+        {
             displayName: 'Sanny Sherief',
             photoUrl: 'https://i.imgur.com/l5YJgol.png',
             address: '123 Fake Street',
             city: 'Los Angeles',
             state: 'CA',
             zip: '90019',
+            phone: '3233233232',
         },
     ];
 
     return (
         <>
             <h1>All Technicians</h1>
-            <ul className={styles.techs}>
-                {techs.map((tech, index) => {
-                    return (
-                        <li className={styles.techWrapper}>
-                            <div className={styles.tech}>
-                                <TechCard key={index} {...tech} />
-                            </div>
-                        </li>
-                    )
-                })}
-            </ul>
+            <Grid container xs={12} spacing={3} >
+                {
+                    techs.map((tech, index) => {
+                        return (
+                            <Grid item sm={12} md={6} key={index}>
+                                <TechCard {...tech} />
+                            </Grid>
+                        )
+                    })
+                }
+            </Grid>
         </>
     );
 };
