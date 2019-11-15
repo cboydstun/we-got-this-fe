d12aa92d78fff186dcdebf1c69ba88b290e1d1c1
Nick <nicholasinterest1@icloud.com>
2019-11-14 20:03:25 -0500

    and the styling continues

--- src/components/forms/InviteTech.js
+++ src/components/forms/InviteTech.js
@@ -55,13 +55,13 @@ const InviteTech = () => {
                             Invite Tech
                         </h1>
 
-                            <TextField error={touched.name} onChange={handleChange} name='name' value={values.name} onBlur={handleBlur} className={touched.name && errors.name ? 'has-error' : null} label="Full Name" margin="normal" variant="outlined" />
+                            <TextField error={errors.name && touched.name} onChange={handleChange} name='name' value={values.name} onBlur={handleBlur} className={touched.name && errors.name ? 'has-error' : null} label="Full Name" margin="normal" variant="outlined" />
                             <Error touched={touched.name} message={errors.name} />
 
-                            <TextField error={touched.email} onChange={handleChange} name='email' value={values.email} onBlur={handleBlur} className={touched.email && errors.email ? 'has-error' : null} label="Email" margin="normal" variant="outlined" />
+                            <TextField error={errors.email && touched.email} onChange={handleChange} name='email' value={values.email} onBlur={handleBlur} className={touched.email && errors.email ? 'has-error' : null} label="Email" margin="normal" variant="outlined" />
                             <Error touched={touched.email} message={errors.email} />
 
-                            <TextField error={touched.phone} onChange={handleChange} name='phone' value={values.phone} onBlur={handleBlur} className={touched.phone && errors.phone ? 'has-error' : null} label="Phone Number" margin="normal" variant="outlined" />
+                            <TextField error={errors.phone && touched.phone} onChange={handleChange} name='phone' value={values.phone} onBlur={handleBlur} className={touched.phone && errors.phone ? 'has-error' : null} label="Phone Number" margin="normal" variant="outlined" />
                             <Error touched={touched.phone} message={errors.phone} />
 
                             <TextareaAutosize onChange={handleChange} name='notes' placeholder='Notes' value={values.notes} onBlur={handleBlur} className={touched.notes && errors.notes ? 'has-error' : null} />
