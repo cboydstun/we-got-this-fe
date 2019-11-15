14dde25aa818932625b4db67142fc3b129d4eaf0
Nick <nicholasinterest1@icloud.com>
2019-11-15 18:28:11 -0500

    refactoring styling and layout for invite tech form

--- src/components/forms/InviteTech.js
+++ src/components/forms/InviteTech.js
@@ -1,90 +1,154 @@
-import React from 'react';
+import React from "react";
 import { Formik } from "formik";
+import { actions } from "../../state/auth/authActions";
+import { useStateValue } from "../../state";
 import * as Yup from "yup";
-import Error from '../Error';
-import { TextField } from '@material-ui/core';
-// import TextareaAutoSizing from '@material-ui/core/TextareaAutosize';
-import { makeStyles } from '@material-ui/core';
+import Error from "../Error";
+import { TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+import SaveIcon from "@material-ui/icons/Save";
+import Button from "@material-ui/core/Button";
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 const validationSchema = Yup.object().shape({
-    name: Yup.string()
-        .required('Full Name Required'),
+  name: Yup.string().required("Full Name Required"),
 
-    email: Yup.string()
-        .required('Email Required'),
+  email: Yup.string().required("Email Required"),
 
-    phone: Yup.number()
-        .required('Phone Number Required'),
+  teamAssignment: Yup.string().required("Team Assignment Required")
+});
 
-    notes: Yup.string()
-        .max(255, 'Text in this field must be shorter than 255 characters')
-})
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1)
+  },
 
-const useStyles = makeStyles({
-    root: {
-        display: "none",
-    }
-  });
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 225,
+    alignItems: "center"
+  },
+
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+
+}));
 
 const InviteTech = () => {
-    
-    const classes = useStyles();
+  const [state, dispatch] = useStateValue();
+  const [teamAssignment, setTeamAssignment] = React.useState('');
+  const inputLabel = React.useRef(null);
+  const [labelWidth, setLabelWidth] = React.useState(0);
+  React.useEffect(() => {
+    setLabelWidth(inputLabel.current);
+  }, []);
+  const classes = useStyles();
 
-    return (
-        <Formik
-            initialValues={{
-                name: '',
-                email: '',
-                phone: '',
-                notes: '',
-            }}
+  return (
+    <Formik
+      initialValues={{
+        name: "",
+        email: "",
+        teamAssignment: ""
+      }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { setSubmitting, resetForm }) => {
+        setSubmitting(true);
+        actions.inviteTech(dispatch, values).then(res => {
+          console.log("RES...", res);
+        });
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          resetForm();
+          setSubmitting(false);
+          console.log("SS..", setSubmitting);
+        }, 500);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting
+      }) => (
+        <form className="Form" onSubmit={handleSubmit}>
+          <h1>Invite Tech</h1>
 
-            validationSchema={validationSchema}
-            onSubmit={(values, { setSubmitting, resetForm }) => {
-                setSubmitting(true)
+          <TextField
+            error={errors.name && touched.name}
+            onChange={handleChange}
+            name="name"
+            value={values.name}
+            onBlur={handleBlur}
+            className={touched.name && errors.name ? "has-error" : null}
+            label="Full Name"
+            margin="normal"
+            variant="outlined"
+          />
+          <Error touched={touched.name} messteamAssignment={errors.name} />
 
-                actions.inviteTech(dispatch, values)
-                .then(res => {
-                    console.log('RES...' , res)
-                })
+          <TextField
+            error={errors.email && touched.email}
+            onChange={handleChange}
+            name="email"
+            value={values.email}
+            onBlur={handleBlur}
+            className={touched.email && errors.email ? "has-error" : null}
+            label="Email"
+            margin="normal"
+            variant="outlined"
+          />
+          <Error touched={touched.email} messteamAssignment={errors.email} />
 
-                setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
-                    resetForm();
-                    setSubmitting(false);
-                    console.log('SS..', setSubmitting)
-                }, 500);
-            }}>
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel className={classes.dropdown} ref={inputLabel} id="demo-simple-select-outlined-label">
+              Team Assignment
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+              value={teamAssignment}
+              onChange={handleChange}
+              labelWidth={labelWidth}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Team A</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
 
-    
-            {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
-            
-                <form className='Form' onSubmit={handleSubmit}>
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            className={classes.button}
+          >
+            Cancel
+          </Button>
 
-                        <h1>
-                            Invite Tech
-                        </h1>
-
-                            <TextField error={errors.name && touched.name} onChange={handleChange} name='name' value={values.name} onBlur={handleBlur} className={touched.name && errors.name ? 'has-error' : null} label="Full Name" margin="normal" variant="outlined" />
-                            <Error touched={touched.name} message={errors.name} />
-
-                            <TextField error={errors.email && touched.email} onChange={handleChange} name='email' value={values.email} onBlur={handleBlur} className={touched.email && errors.email ? 'has-error' : null} label="Email" margin="normal" variant="outlined" />
-                            <Error touched={touched.email} message={errors.email} />
-
-                            <TextField error={errors.phone && touched.phone} onChange={handleChange} name='phone' value={values.phone} onBlur={handleBlur} className={touched.phone && errors.phone ? 'has-error' : null} label="Phone Number" margin="normal" variant="outlined" />
-                            <Error touched={touched.phone} message={errors.phone} />
-
-                            <textarea className={classes.root} onChange={handleChange} name='notes' placeholder='Banana' value={values.notes} onBlur={handleBlur} className={touched.notes && errors.notes ? 'has-error' : null} />
-                            <Error touched={touched.notes} message={errors.notes} />
-
-                        <div className='tech-buttons'>
-                            <button id='tech-cancel'>Cancel</button>
-                            <button type='submit' disabled={isSubmitting}>Submit</button>
-                        </div> 
-
-                </form> /* Form end */
-            )}
-        </Formik>
-    )
-}
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            className={classes.button}
+            startIcon={<SaveIcon />}
+          >
+            Save
+          </Button>
+        </form> /* Form end */
+      )}
+    </Formik>
+  );
+};
 export default InviteTech;
