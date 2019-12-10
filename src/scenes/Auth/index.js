d30e37bdcf8d23fafd83cfaacfb54ec55bf26f7f
sanny-io <sannysherief@gmail.com>
2019-12-10 15:39:55 -0800

    Fix startup errors.

--- src/scenes/Auth/index.js
+++ src/scenes/Auth/index.js
@@ -23,8 +23,8 @@ const useStyles = makeStyles({
   },
 
   root4: {
-      borderRadius: props => props.borderRadius,
-      boxShadow: props => props.boxShadow
+    borderRadius: props => props.borderRadius,
+    boxShadow: props => props.boxShadow
   }
 });
 
@@ -38,7 +38,7 @@ const Auth = () => {
     <>
       <Row>
         <Column>
-            <div className={`${classes.root3}`}><img src={images} /></div>
+          <div className={`${classes.root3}`}><img src={images} /></div>
         </Column>
         <Column justify="center" align="center" padding={20}>
           <h1 className={`${classes.root}`}>Welcome back!</h1>
@@ -46,8 +46,8 @@ const Auth = () => {
           {auth.loadingUser ? (
             <h4>Loading</h4>
           ) : (
-            <h4>{auth.currentUser && auth.currentUser.displayName}</h4>
-          )}
+              <h4>{auth.currentUser && auth.currentUser.displayName}</h4>
+            )}
           <button className={`${classes.root4}`}
             onClick={async () => {
               let result = await actions.login(dispatch);
@@ -75,48 +75,6 @@ const Auth = () => {
       </Row>
     </>
   );
-=======
-import React from 'react';
-import { useStateValue } from '../../state';
-import { actions } from '../../state/auth/authActions';
-import Column from '../../components/styles/containers/Column';
-import Row from '../../components/styles/containers/Row';
-
-const Auth = () => {
-    const [{ auth }, dispatch] = useStateValue();
-    return (
-        <>
-            <Row>
-                <Column justify="center" align="center" padding={20}>
-                    <h1>Auth</h1>
-                    {auth.loadingUser ? (
-                        <h4>Loading</h4>
-                    ) : (
-                        <h4>
-                            {auth.currentUser && auth.currentUser.displayName}
-                        </h4>
-                    )}
-                    <button
-                        onClick={async () => {
-                            await actions.login(dispatch);
-                        }}
-                    >
-                        Sign In With Google
-                    </button>
-                    <button
-                        onClick={async () => {
-                            await actions.login(dispatch);
-                        }}
-                    >
-                        Register With Google
-                    </button>
-                </Column>
-                <Column justify="center" align="center" padding={20}>
-                    <div>Here is some text!</div>
-                </Column>
-            </Row>
-        </>
-    );
 };
 
 export default Auth;
