fe26a9232f21ad144ecf4e26ea3419e66dee237a
Tashi Woeser <tashiwoeser@gmail.com>
2019-12-10 14:25:46 -0800

    styling login page

--- src/scenes/Auth/index.js
+++ src/scenes/Auth/index.js
@@ -8,47 +8,74 @@ import Row from "../../components/styles/containers/Row";
 import { makeStyles } from "@material-ui/core/styles";
 import images from "../../images/loginPic.png";
 
-const useStyles = makeStyles({
-  root: props => ({
-    color: props.color,
-    fontWeight: props.fontWeight
-  }),
 
-  root2: {
-    color: props => props.color
-  },
+const useStyles = makeStyles(theme => ({
+    main: {
+        border: '1px solid black',
+        padding: '0'
+    },
+    loginImage: {
+        backgroundColor: '#2678C0',
+    },
+    image: {
+        width: '770px',
+        height: '877px',
+        padding: '66px',
+    },
+    text1: {
+        textAlign: 'center',
+        margin: 'auto',
+        color: '#2678C0',
+        fontSize: '50px',
+        paddingTop: '203px',
+    },
+    text2: {
+        margin: 'auto',
+        color: '#2678C0',
+        fontSize: '35px',
+    },
+    googleLogo: {
+        height: '47px',
+        width: '50px',
+        paddingTop: '3px',
+    },
+    buttonSignIn: {
+        height: '50px',
+        width: '240px',
+        backgroundColor: 'white',
+        margin: 'auto',
+        border: '1px solid white'
+    },
+    flex: {
+        display: 'flex',
+        border: '1px solid black'
+    }
 
-  root3: {
-    backgroundColor: props => props.backgroundColor,
-  },
-
-  root4: {
-      borderRadius: props => props.borderRadius,
-      boxShadow: props => props.boxShadow
-  }
-});
+   
+}));
 
 const Auth = () => {
   const [{ auth }, dispatch] = useStateValue();
   const history = useHistory();
-  const props = { color: "#2678C0", fontWeight: "bold", backgroundColor: "#2678C0", borderRadius: "4px", backgroundSize: "100%", boxShadow: "10px" };
-  const classes = useStyles(props);
+  const classes = useStyles();
 
   return (
-    <>
+    <div className={classes.main}>
       <Row>
-        <Column>
-            <div className={`${classes.root3}`}><img src={images} /></div>
+        <Column className={classes.zeroPadding}>
+            <div className={classes.loginImage}><img className={classes.image} src={images} /></div>
         </Column>
-        <Column justify="center" align="center" padding={20}>
-          <h1 className={`${classes.root}`}>Welcome back!</h1>
-          <h2 className={`${classes.root2}`}>We know you got this!</h2>
+        <Column>
+          <h1 className={classes.text1}>Welcome back!</h1>
+          <h2 className={classes.text2}>We know you got this!</h2>
           {auth.loadingUser ? (
             <h4>Loading</h4>
           ) : (
             <h4>{auth.currentUser && auth.currentUser.displayName}</h4>
           )}
-          <button className={`${classes.root4}`}
+          <div className={classes.flex}>
+          <img className={classes.googleLogo} src='https://www.sketchappsources.com/resources/source-image/google-g-logo.jpg'></img>
+          <button className={classes.buttonSignIn} 
             onClick={async () => {
               let result = await actions.login(dispatch);
               console.log(result);
@@ -59,6 +86,7 @@ const Auth = () => {
           >
             Sign In With Google
           </button>
+          </div>
           <div>Don't have an account?</div>
           <button
             onClick={async () => {
@@ -73,50 +101,8 @@ const Auth = () => {
           </button>
         </Column>
       </Row>
-    </>
+    </div>
   );
-=======
-import React from 'react';
-import { useStateValue } from '../../state';
-import { actions } from '../../state/auth/authActions';
-import Column from '../../components/styles/containers/Column';
-import Row from '../../components/styles/containers/Row';
-
-const Auth = () => {
-    const [{ auth }, dispatch] = useStateValue();
-    return (
-        <>
-            <Row>
-                <Column justify="center" align="center" padding={20}>
-                    <h1>Auth</h1>
-                    {auth.loadingUser ? (
-                        <h4>Loading</h4>
-                    ) : (
-                        <h4>
-                            {auth.currentUser && auth.currentUser.displayName}
-                        </h4>
-                    )}
-                    <button
-                        onClick={async () => {
-                            await actions.login(dispatch);
-                        }}
-                    >
-                        Sign In With Google
-                    </button>
-                    <button
-                        onClick={async () => {
-                            await actions.login(dispatch);
-                        }}
-                    >
-                        Register With Google
-                    </button>
-                </Column>
-                <Column justify="center" align="center" padding={20}>
-                    <div>Here is some text!</div>
-                </Column>
-            </Row>
-        </>
-    );
 };
 
 export default Auth;
