f04ddaace287fd93d1182906c0e6057a95430178
Lucas Bazemore <lucasabazemore@gmail.com>
2019-12-11 00:46:05 -0600

    check the testing

--- cypress/integration/wgtCustomerSpec.js
+++ cypress/integration/wgtCustomerSpec.js
@@ -1,55 +1,50 @@
-describe('We Got This Test', function () {
-    it('Test Customer Page Functionality', function () {
-        cy.visit('/customers')
+describe('We Got This Test', function() {
+    it('Test Customer Page Functionality', function() {
+        cy.visit('/customers');
 
         // cy.pause()
 
-        cy.contains('Customers').click()
+        // cy.contains('Customers').click()
 
-        cy.url()
-        .should('include', '/customers')
+        // cy.url()
+        // .should('include', '/customers')
 
-        cy.contains('Sanny').click()
+        // cy.contains('Sanny').click()
+        cy.url().should('include', '/customers');
+    });
+});
 
-        
-    })
-} )
+// describe('We Got This Test', function () {
 
-describe('We Got This Test', function () {
-        
-    it('Test Customer Page Functionality', function () {
-        cy.visit('/customers')
-// cy.fixture("newCustomer").as("newCustomer");
-        // cy.pause()
+//     it('Test Customer Page Functionality', function () {
+//         cy.visit('/customers')
+// // cy.fixture("newCustomer").as("newCustomer");
+//         // cy.pause()
 
-        cy.contains('Customers').click()
+//         cy.contains('Customers').click()
 
-        cy.url()
-        .should('include', '/customers')
+//         cy.url()
+//         .should('include', '/customers')
 
-        cy.contains('New Customer').click()
+//         cy.contains('New Customer').click()
 
-    //     cy
-    //     .get('input[id="name"]')
-    //     .type(this.newCustomer.name)
-    //     .should("have.value", this.newCustomer.name);
-    //   cy
-    //     .get('input[id="email"]')
-    //     .type(this.newCustomer.email)
-    //     .should("have.value", this.newCustomer.email);
-    //   cy
-    //     .get('input[id="phoneNumber"]')
-    //     .type(this.newCustomer.phoneNumber)
-    //     .should("have.value", this.newCustomer.phoneNumber);
-    //   cy
-    //     .get('input[id="street"]')
-    //     .type(this.newCustomer.street)
-    //     .should("have.value", this.newCustomer.street);
-      
-    //   cy.get("#NewCustomerForm").submit();
-    })
-} )
+//     //     cy
+//     //     .get('input[id="name"]')
+//     //     .type(this.newCustomer.name)
+//     //     .should("have.value", this.newCustomer.name);
+//     //   cy
+//     //     .get('input[id="email"]')
+//     //     .type(this.newCustomer.email)
+//     //     .should("have.value", this.newCustomer.email);
+//     //   cy
+//     //     .get('input[id="phoneNumber"]')
+//     //     .type(this.newCustomer.phoneNumber)
+//     //     .should("have.value", this.newCustomer.phoneNumber);
+//     //   cy
+//     //     .get('input[id="street"]')
+//     //     .type(this.newCustomer.street)
+//     //     .should("have.value", this.newCustomer.street);
 
-
-
-
+//     //   cy.get("#NewCustomerForm").submit();
+//     })
+// } )
