a633738dc1663350512951b5acea2b04a1aeb0c9
Lucas Bazemore <lucasabazemore@gmail.com>
2020-01-06 18:13:09 -0600

    Styling/login (#53)
    * Styling/create customer form (#13)
    
    LB: Redirect form on success. Updated form names
    
    * Set up Error details for input fields for register company
    
    * added error handling and reset
    
    * Scene/onboarding (#11)
    
    * on new account redirect to company form
    
    * move logout to Profile, and redirect to dashboard on login
    
    * redirect create company on success
    
    * added row and column components
    
    * Fix imports.
    
    * Initial work.
    
    * More tech work.
    
    * Complete techs grid.
    
    * Remove unnecessary imports.
    
    * Header progress.
    
    * style updating
    
    * update styling
    
    * worked with Sanny and Nick for tech to team adding
    
    *  customerform submit works but still need to add to db
    
    * Component/jobs (#12)
    
    LB: Will need to be a full component later, but for now it's on hiatus
    
    * working on jobscards
    
    * fine tuning the jobscards
    
    * finishing jobCards
    
    * jobCards
    
    * Almost done with Jobs Cards
    
    * Figuring things out
    
    * Pausing this for now
    
    * remove JobsList from Dashboard for now
    
    * get form to work and redirect properly
    
    * Revert "Styling/create customer form (#13)" (#18)
    
    This reverts commit c33d0693634bc23881785bd6eb7e2ce43f18866f.
    
    * remove login as Scott
    
    * update package.json
    
    * remove snapshots
    
    * added tests for CustomersTable
    
    * cleanup login page
    
    Co-authored-by: Michael A Demery <52550247+DemeryMA@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+	setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+	moduleDirectories: ['node_modules', 'test'],
 };
