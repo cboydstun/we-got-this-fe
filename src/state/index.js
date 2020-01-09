ec8acfbb3bffe02a2feb39b6d43e09fe8909131a
Lucas Bazemore <lucasabazemore@gmail.com>
2020-01-08 20:31:12 -0600

    clean up

--- src/state/index.js
+++ src/state/index.js
@@ -1,66 +1,66 @@
-import React, { createContext, useContext, useReducer } from 'react';
+import React, {createContext, useContext, useReducer} from 'react';
 
 export const StateContext = createContext();
 
-export const StateProvider = ({ reducer, initialState, children }) => {
-    return (
-        <StateContext.Provider value={useReducer(reducer, initialState)}>
-            {children}
-        </StateContext.Provider>
-    );
+export const StateProvider = ({reducer, initialState, children}) => {
+	return (
+		<StateContext.Provider value={useReducer(reducer, initialState)}>
+			{children}
+		</StateContext.Provider>
+	);
 };
 
 const createActionName = functionName =>
-    functionName.replace(/[A-Z]/g, match => `_${match}`).toUpperCase();
+	functionName.replace(/[A-Z]/g, match => `_${match}`).toUpperCase();
 const createActionNames = functionName => {
-    return {
-        start: `${createActionName(functionName)}_START`,
-        success: `${createActionName(functionName)}_SUCCESS`,
-        error: `${createActionName(functionName)}_ERROR`,
-    };
+	return {
+		start: `${createActionName(functionName)}_START`,
+		success: `${createActionName(functionName)}_SUCCESS`,
+		error: `${createActionName(functionName)}_ERROR`,
+	};
 };
 
 export const installActionNames = service => {
-    for (const [key, value] of Object.entries(service)) {
-        const { start, success, error } = createActionNames(key);
+	for (const [key, value] of Object.entries(service)) {
+		const {start, success, error} = createActionNames(key);
 
-        value.start = start;
-        value.success = success;
-        value.error = error;
-    }
+		value.start = start;
+		value.success = success;
+		value.error = error;
+	}
 
-    return service;
+	return service;
 };
 
 export const useService = (service, dispatch) => {
-    const wrapper = {};
+	const wrapper = {};
 
-    for (const [key, value] of Object.entries(service)) {
-        wrapper[key] = async (...others) => {
-            dispatch({ type: value.start, payload: others });
+	for (const [key, value] of Object.entries(service)) {
+		wrapper[key] = async (...others) => {
+			dispatch({type: value.start, payload: others});
 
-            try {
-                const result = await value(...others);
+			try {
+				const result = await value(...others);
 
-                dispatch({ type: value.success, payload: result });
+				dispatch({type: value.success, payload: result});
 
-                return result;
-            } catch (error) {
-                dispatch({ type: value.error, payload: error });
-            }
-        };
-    }
+				return result;
+			} catch (error) {
+				dispatch({type: value.error, payload: error});
+			}
+		};
+	}
 
-    return wrapper;
+	return wrapper;
 };
 
 export const useStateValue = () => useContext(StateContext);
 
 export const withState = WrappedComponent => {
-    return props => {
-        const [state, dispatch] = useStateValue();
-        return (
-            <WrappedComponent state={state} dispatch={dispatch} {...props} />
-        );
-    };
+	return props => {
+		const [state, dispatch] = useStateValue();
+		return (
+			<WrappedComponent state={state} dispatch={dispatch} {...props} />
+		);
+	};
 };
